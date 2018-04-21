164e97a4f83259218f15f59893908a55cc2ba80b
chrhong <hongguochun@hotmail.com>
2018-04-21 23:17:25 +0800

    aliens attach game v1.0

---
+++ alien_invasion.py
@@ -0,0 +1,34 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from game_stats import GameStats
+from button import Button
+import game_functions as gf
+from pygame.sprite import Group
+
+def run_game():
+    pygame.init()
+
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    stats = GameStats(ai_settings)
+    ship = Ship(ai_settings, screen)
+    play_button = Button(ai_settings, screen, "Play")
+    bullets = Group()
+    aliens =  Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    while True:
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+
+run_game()
